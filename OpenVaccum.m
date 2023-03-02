cd27ce61d6b3732dd4c001fe7e13ca4f2899cb85
Arman Barghi <arman.barghi@gmail.com>
2023-03-02 20:45:29 +0330

    Functions for take and drop

---
+++ OpenVaccum.m
@@ -0,0 +1,5 @@
+function OpenVaccum(id,vrep,Cuboid) 
+    vrep.simxSetObjectIntParameter(id,Cuboid,3003,0,vrep.simx_opmode_oneshot);
+    vrep.simxSetObjectParent(id,Cuboid,-1,true,vrep.simx_opmode_oneshot);
+end
+
